97b8138194ddaf8a908e19ba94adcd802324f61b
Daeun Jeong <jungde1345@gmail.com>
2024-05-28 15:41:58 +0900

    update

--- src/main/java/com/tickethub/backend/user/security/SecurityConfig.java
+++ src/main/java/com/tickethub/backend/user/security/SecurityConfig.java
@@ -54,8 +54,8 @@ public class SecurityConfig {
                         public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                             CorsConfiguration configuration = new CorsConfiguration();
 
-                            //configuration.setAllowedOrigins(Collections.singletonList("http://k8s-default-prodingr-fc22219cb1-577453725.ap-northeast-2.elb.amazonaws.com"));
-                            configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                            configuration.setAllowedOrigins(Collections.singletonList("http://tickethubs.net"));
+                            //configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
                             configuration.setAllowedMethods(Collections.singletonList("*"));
                             configuration.setAllowCredentials(true);
                             configuration.setAllowedHeaders(Collections.singletonList("*"));
